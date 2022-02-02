28404e553c4ee61b8d57ca3620b49fa9682e6bf4
josevelaz <jose.carlos.velazquez@hotmail.com>
2022-02-02 17:48:25 -0500

    Added server StatusPill.vue

--- vue/views/Home.vue
+++ vue/views/Home.vue
@@ -1,29 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <p>{{ message }}</p>
+    <span class="font-bold">Server Status:</span>
+    <StatusPill text="Operational" :status="status === 200 ? 'success' : 'error'" />
+    <img alt="Vue logo" src="../assets/logo.png" class="mx-auto">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /vue
+import {defineComponent} from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue';
+import StatusPill from "@/components/StatusPill.vue"; // @ is an alias to /vue
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
+    StatusPill
   },
   data() {
     return {
-      message: ''
+      status: 500
     }
   },
   async created() {
-    const response = await fetch("http://localhost:3000")
-    const json = await response.json()
-    this.message = json.message
+    const response = await fetch("http://localhost:3000/health")
+    this.status = response.status
   }
 });
 </script>
