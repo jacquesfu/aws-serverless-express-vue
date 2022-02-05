0a6de7e5cfd16a0606b44f59c42a81fbe695823d
josevelaz <jose.carlos.velazquez@hotmail.com>
2022-02-04 20:01:56 -0500

    added response to status route

--- express/routes/status-routes.ts
+++ express/routes/status-routes.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  return res.status(200).json({});
+  return res.status(200).json({ message: "Message from the server" });
 });
 
 export default router;
