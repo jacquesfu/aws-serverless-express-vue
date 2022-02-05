313c70cd8841cf8a7061d12af4aa8db184c4fa88
josevelaz <jose.carlos.velazquez@hotmail.com>
2022-02-04 20:07:42 -0500

    added CodeBlock.vue

---
+++ vue/components/CodeBlock.vue
@@ -0,0 +1,18 @@
+<template>
+  <pre>
+    <code class="whitespace-pre bg-zinc-50 block max-w-lg mx-auto flex border-solid border border-gray-100">
+      {{ code }}
+    </code>
+  </pre>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CodeBlock",
+  props: {
+    code: String
+  }
+});
+</script>
