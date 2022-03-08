0f8f56eef4179fcc13fb9f3cd3c2a17ce8b34401
josevelaz <jose.carlos.velazquez@hotmail.com>
2022-03-08 18:15:39 -0500

    added firebase login UI and /login route

--- express/routes/status-routes.ts
+++ express/routes/status-routes.ts
@@ -3,7 +3,9 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  return res.status(200).json({ message: "Welcome from Express!" });
+  return res
+    .status(200)
+    .json({ message: `Welcome from ${process.env.ENV_MESSAGE}!` });
 });
 
 export default router;
